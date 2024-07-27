770e2d0a2ba63f4fe57418e6da2ca47d30659526
kibo13 <kimboris1310@gmail.com>
2024-07-27 14:20:31 +0500

    feat: add the logic of sending an application to firebase

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { addData } from '../services/firebase/firebase.utils';
 
 const ContactForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        topic: 'idea',
+        email: '',
+        description: '',
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [id]: value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await addData('orders', formData);
+            alert('Заявка успешно отправлена!');
+            setFormData({
+                name: '',
+                topic: 'idea',
+                email: '',
+                description: '',
+            });
+        } catch (error) {
+            console.error('Error submitting form: ', error);
+            alert('Ошибка при отправке заявки. Пожалуйста, попробуйте еще раз.');
+        }
+    };
+
     return (
         <section className="contact section container" id="contact">
             <h2 className="section__title">
                 Подать заявку
             </h2>
-            <form action="#" className="contact__form">
+            <form onSubmit={handleSubmit} className="contact__form">
                 <div className="contact__form-group">
                     <label htmlFor="name" className="contact__form-label">Представьтесь, пожалуйста!</label>
-                    <input type="text" id="name" className="contact__form-input" required />
+                    <input
+                        type="text"
+                        id="name"
+                        className="contact__form-input"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
                 <div className="contact__form-group">
                     <label htmlFor="email" className="contact__form-label">Ваш email для связи</label>
-                    <input type="email" id="email" className="contact__form-input" required />
+                    <input
+                        type="email"
+                        id="email"
+                        className="contact__form-input"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
                 <div className="contact__form-group">
                     <label htmlFor="topic" className="contact__form-label">Какова ваша цель?</label>
-                    <select id="topic" className="contact__form-input" required>
+                    <select
+                        id="topic"
+                        className="contact__form-input"
+                        value={formData.topic}
+                        onChange={handleChange}
+                        required
+                    >
+                        <option value="" disabled>Выберите</option>
+                        {/* Скрытая опция */}
                         <option value="idea">Я новатор, у меня есть идея</option>
                         <option value="partnership">Меня интересует партнерство</option>
                     </select>
                 </div>
                 <div className="contact__form-group">
                     <label htmlFor="description" className="contact__form-label">Расскажите о вашей идее</label>
-                    <textarea id="description" className="contact__form-textarea" required></textarea>
+                    <textarea
+                        id="description"
+                        className="contact__form-textarea"
+                        value={formData.description}
+                        onChange={handleChange}
+                        required
+                    ></textarea>
                 </div>
                 <button type="submit" className="contact__form-button">Отправить</button>
             </form>
