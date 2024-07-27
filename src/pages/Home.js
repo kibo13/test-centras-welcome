5e7f94744bbff9f580e925d6e44dd6892f6dc3cc
kibo13 <kimboris1310@gmail.com>
2024-07-27 15:46:21 +0500

    refactor: move layout from App.js file into separate Home.js file

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Main from '../components/Main';
+import ContactForm from '../components/ContactForm';
+import Footer from '../components/Footer';
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <ContactForm />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
