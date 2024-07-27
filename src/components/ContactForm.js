78136a72ca7f3c56a6fe9bf56c08da91855659fc
kibo13 <kimboris1310@gmail.com>
2024-07-27 14:07:00 +0500

    chore: updated the text in the label tags

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,22 +8,22 @@ const ContactForm = () => {
             </h2>
             <form action="#" className="contact__form">
                 <div className="contact__form-group">
-                    <label htmlFor="name" className="contact__form-label">Имя и Фамилия</label>
+                    <label htmlFor="name" className="contact__form-label">Представьтесь, пожалуйста!</label>
                     <input type="text" id="name" className="contact__form-input" required />
                 </div>
                 <div className="contact__form-group">
-                    <label htmlFor="email" className="contact__form-label">Email</label>
+                    <label htmlFor="email" className="contact__form-label">Ваш email для связи</label>
                     <input type="email" id="email" className="contact__form-input" required />
                 </div>
                 <div className="contact__form-group">
-                    <label htmlFor="topic" className="contact__form-label">Тематика</label>
+                    <label htmlFor="topic" className="contact__form-label">Какова ваша цель?</label>
                     <select id="topic" className="contact__form-input" required>
-                        <option value="idea">Идея</option>
-                        <option value="partnership">Партнерство</option>
+                        <option value="idea">Я новатор, у меня есть идея</option>
+                        <option value="partnership">Меня интересует партнерство</option>
                     </select>
                 </div>
                 <div className="contact__form-group">
-                    <label htmlFor="description" className="contact__form-label">Описание идеи</label>
+                    <label htmlFor="description" className="contact__form-label">Расскажите о вашей идее</label>
                     <textarea id="description" className="contact__form-textarea" required></textarea>
                 </div>
                 <button type="submit" className="contact__form-button">Отправить</button>
