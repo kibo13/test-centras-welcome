0540232e278fc16cf7174af956c4f36a4c0886b6
kibo13 <kimboris1310@gmail.com>
2024-07-27 13:44:43 +0500

    rollback: remove bootstrap's dependencies from index.js file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
